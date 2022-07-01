d488b91479bebada56af652f9761de32fb0d9218
Christoph Ketzler <christoph@ketzler.de>
2022-07-01 09:33:04 +0200

    Added remote control over 433Mhz Module

--- LockButton.ino
+++ LockButton.ino
@@ -1,31 +1,44 @@
 #include <JC_Button.h>              // https://github.com/JChristensen/JC_Button
 //#include "Keyboard.h"
 
-Button btnA(19,100);    // define the buttons
-
+Button btnBig(19,100);    // define the buttons
+Button btnA(10,100,true,false);
+Button btnB(9,100,true,false);
+Button btnC(15,100,true,false);
+Button btnD(16,100,true,false);
 
 void setup(){
-  Serial.begin(115200);
+  //Serial.begin(115200);
  
+   btnBig.begin();
    btnA.begin();
+   btnB.begin();
+   btnC.begin();
+   btnD.begin();
    pinMode(18,OUTPUT);
+
+   pinMode(9, INPUT_PULLUP);
    
    //Keyboard.begin();
 }
 
 void loop(){
   
-  btnA.read();                   // read the buttons
+  btnBig.read();                   // read the buttons
+  btnA.read();
+  btnB.read();
+  btnC.read();
+  btnD.read();
   
-  if (btnA.isPressed())
+  if (btnBig.isPressed())
   {
      digitalWrite(18,HIGH);
   } else {
     digitalWrite(18,LOW);
   }
-  if (btnA.wasPressed())
+  if (btnBig.wasPressed() || btnA.wasPressed())
   {
-    Serial.println("A");
+    //Serial.println("Lock");
     //Keyboard.press('a');
     //Keyboard.releaseAll();
     
@@ -40,6 +53,59 @@ void loop(){
     Keyboard.send_now();
     //Keyboard.release(MODIFIERKEY_GUI);
     
+  }
+  if (btnBig.wasPressed() || btnA.wasPressed())
+  {
+    //Serial.println("Lock");
+    //Keyboard.press('a');
+    //Keyboard.releaseAll();
+    
+    //Keyboard.press(MODIFIERKEY_GUI);
+    Keyboard.set_modifier(MODIFIERKEY_GUI);
+    Keyboard.set_key1(KEY_L);
+    Keyboard.send_now();
+    //Keyboard.print("l");
+    delay(25);
+    Keyboard.set_modifier(0);
+    Keyboard.set_key1(0);
+    Keyboard.send_now();
+    //Keyboard.release(MODIFIERKEY_GUI);
+    
+  }
+
+  if (btnB.wasPressed())
+  {
+    //Serial.println("B: Teams Mute");
+    Keyboard.set_modifier(MODIFIERKEY_CTRL | MODIFIERKEY_SHIFT);
+    Keyboard.set_key1(KEY_M);
+    Keyboard.send_now();
+    delay(25);
+    Keyboard.set_modifier(0);
+    Keyboard.set_key1(0);
+    Keyboard.send_now();
+  }
+
+  if (btnC.wasPressed())
+  {
+    //Serial.println("C: Teams Camera");
+    Keyboard.set_modifier(MODIFIERKEY_CTRL | MODIFIERKEY_SHIFT);
+    Keyboard.set_key1(KEY_O);
+    Keyboard.send_now();
+    delay(25);
+    Keyboard.set_modifier(0);
+    Keyboard.set_key1(0);
+    Keyboard.send_now();
+  }
+  if (btnD.wasPressed())
+  {
+    //Serial.println("D: End Call");
+    Keyboard.set_modifier(MODIFIERKEY_CTRL | MODIFIERKEY_SHIFT);
+    Keyboard.set_key1(KEY_H);
+    Keyboard.send_now();
+    delay(25);
+    Keyboard.set_modifier(0);
+    Keyboard.set_key1(0);
+    Keyboard.send_now();
   }
   
 }
